cad56cffc65878a3a9ee664e9c902dc8f89c545b
G3N <fengyuan.yap@gmail.com>
2022-07-19 22:21:22 +0900

    created a mid column for coin row

--- CryptoChecker/Core/Home/Views/CoinRowView.swift
+++ CryptoChecker/Core/Home/Views/CoinRowView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct CoinRowView: View {
   let coin: CoinModel
+  let showHoldingColumn: Bool
   
   var body: some View {
+    HStack(spacing: 0) {
+      leftColumn
+      
+      Spacer()
+      
+      if showHoldingColumn {
+        centerColumn
+      }
+      
+      rightColumn
+    } //: HSTACK
+    .font(.subheadline)
+  }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+  static var previews: some View {
+    Group {
+      CoinRowView(coin: dev.coin, showHoldingColumn: true)
+        .previewLayout(.sizeThatFits)
+      .padding()
+      CoinRowView(coin: dev.coin, showHoldingColumn: true)
+        .preferredColorScheme(.dark)
+        .previewLayout(.sizeThatFits)
+        .padding()
+    }
+  }
+}
+
+extension CoinRowView {
+  private var leftColumn: some View {
     HStack(spacing: 0) {
       Text("\(coin.rank)")
         .font(.caption)
@@ -24,30 +56,30 @@ struct CoinRowView: View {
         .padding(.leading, 6)
         .font(.headline)
         .foregroundColor(.theme.accent)
-      
-      Spacer()
-      
-      VStack(alignment: .trailing) {
-        Text(coin.currentPrice.asCurrencyWith6Decimals())
-          .foregroundColor(.theme.accent)
-          .fontWeight(.bold)
-        
-        Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
-          .foregroundColor(
-            (coin.priceChangePercentage24H ?? 0) >= 0 ?
-              .theme.green :
-              .theme.red
-          )
-        
-      } //: VSTACK
     } //: HSTACK
   }
-}
-
-struct CoinRowView_Previews: PreviewProvider {
-  static var previews: some View {
-    CoinRowView(coin: dev.coin)
-      .previewLayout(.sizeThatFits)
-      .padding()
+  
+  private var centerColumn: some View {
+    VStack(alignment: .trailing) {
+      Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+      Text((coin.currentHoldings ?? 0).asNumberString())
+    }
+    .foregroundColor(.theme.accent)
+  }
+  
+  private var rightColumn: some View {
+    VStack(alignment: .trailing) {
+      Text(coin.currentPrice.asCurrencyWith6Decimals())
+        .foregroundColor(.theme.accent)
+        .fontWeight(.bold)
+      
+      Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+        .foregroundColor(
+          (coin.priceChangePercentage24H ?? 0) >= 0 ?
+            .theme.green :
+            .theme.red
+        )
+    } //: VSTACK
+    .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
   }
 }
--- CryptoChecker/Extensions/Double.swift
+++ CryptoChecker/Extensions/Double.swift
@@ -8,6 +8,33 @@
 import Foundation
 
 extension Double {
+  /// Convert a double into a Currency with 2  decimal
+  ///  ```
+  ///  1234.56 -> $1,234.56
+  ///  ```
+  private var currencyFormatter2: NumberFormatter {
+    let formatter = NumberFormatter()
+    
+    formatter.usesGroupingSeparator = true
+    formatter.numberStyle = .currency
+//    formatter.locale = .current
+//    formatter.currencyCode = "usd"
+//    formatter.currencySymbol = "$"
+    formatter.maximumFractionDigits = 2
+    formatter.minimumFractionDigits = 2
+    
+    return formatter
+  }
+  
+  /// Convert a double into a Currency as a String with 2 to 6 decimal
+  ///  ```
+  ///  1234.56 -> "$1,234.56"
+  ///  ```
+  func asCurrencyWith2Decimals() -> String {
+    let number = NSNumber(value: self)
+    return currencyFormatter2.string(from: number) ?? "$0.00"
+  }
+
   /// Convert a double into a Currency with 2 to 6 decimal
   ///  ```
   ///  1234.56 -> $1,234.56
