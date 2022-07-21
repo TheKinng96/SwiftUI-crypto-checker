0ceb1f4dc65c4428c3fe5011552bc28c7ded00c7
G3N <fengyuan.yap@gmail.com>
2022-07-22 07:36:15 +0900

    moved network logic into reusable component

---
+++ CryptoChecker/Utilities/NetworkManager.swift
@@ -0,0 +1,49 @@
+//
+//  NetworkManager.swift
+//  CryptoChecker
+//
+//  Created by Feng Yuan Yap on 2022/07/22.
+//
+
+import Foundation
+import Combine
+
+class NetworkManager {
+  
+  enum NetworkError: LocalizedError {
+    case badURLResponse(url: URL)
+    case unknown
+    
+    var errorDescription: String? {
+      switch self {
+      case .badURLResponse(url: let url): return "Bad URL response: \(url)"
+      case .unknown: return  "Unknown error occur."
+      }
+    }
+  }
+  
+  static func download(url: URL) -> AnyPublisher<Data, Error> {
+    return URLSession.shared.dataTaskPublisher(for: url)
+      .subscribe(on: DispatchQueue.global(qos: .default))
+      .tryMap({ try handleURLResponse(output: $0, url: url) })
+      .receive(on: DispatchQueue.main)
+      .eraseToAnyPublisher()
+  }
+  
+  static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+    guard let response = output.response as? HTTPURLResponse,
+          response.statusCode >= 200 && response.statusCode <= 300 else {
+      throw NetworkError.badURLResponse(url: url)
+    }
+    return output.data
+  }
+  
+  static func handleCompletion(completion: Subscribers.Completion<Error>) {
+    switch completion {
+    case .finished:
+      break
+    case .failure(let error):
+      print(error.localizedDescription)
+    }
+  }
+}
