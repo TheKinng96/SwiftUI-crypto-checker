cec9453a34f94cddb0f67348f62aa68918e02890
G3N <fengyuan.yap@gmail.com>
2022-07-27 08:03:47 +0900

    added sorting feature for coins as in price, rank, and holding value

--- CryptoChecker/Core/Home/Views/HomeView.swift
+++ CryptoChecker/Core/Home/Views/HomeView.swift
@@ -115,12 +115,44 @@ extension HomeView {
   
   private var listHeader: some View {
     HStack {
-      Text("Coin")
+      HStack {
+        Text("Coin")
+        Image(systemName: "chevron.down")
+          .opacity((vm.sortOption == .rank || vm.sortOption == .rankReversed) ? 1.0 : 0.0)
+          .rotationEffect(Angle(degrees: vm.sortOption == .rank ? 0 : 180))
+      }
+      .onTapGesture {
+        withAnimation(.default) {
+          vm.sortOption = vm.sortOption == .rank ? .rankReversed : .rank
+        }
+      }
+
       Spacer()
       if showPortfolio {
-        Text("Holdings")
+        HStack {
+          Text("Holdings")
+          Image(systemName: "chevron.down")
+            .opacity((vm.sortOption == .holdings || vm.sortOption == .holdingsReversed) ? 1.0 : 0.0)
+            .rotationEffect(Angle(degrees: vm.sortOption == .holdings ? 0 : 180))
+        }
+        .onTapGesture {
+          withAnimation(.default) {
+            vm.sortOption = vm.sortOption == .holdings ? .holdingsReversed : .holdings
+          }
+        }
       }
-      Text("Price")
+      
+      HStack {
+        Text("Price")
+        Image(systemName: "chevron.down")
+          .opacity((vm.sortOption == .price || vm.sortOption == .priceReversed) ? 1.0 : 0.0)
+          .rotationEffect(Angle(degrees: vm.sortOption == .price ? 0 : 180))
+      }
+      .onTapGesture {
+        withAnimation(.default) {
+          vm.sortOption = vm.sortOption == .price ? .priceReversed : .price
+        }
+      }
         .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
       
       Button(action: {
